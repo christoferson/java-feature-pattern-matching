1e21387c79ef358b33264e2d3f3cafdd7860a49f
christoferson <85178973+christoferson@users.noreply.github.com>
2021-06-18 00:56:01 +0900

    Add new examples

--- TryFeaturePatternMatching.java
+++ TryFeaturePatternMatching.java
@@ -4,8 +4,53 @@ public class TryFeaturePatternMatching {
 
 	public static void main(String[] args) {
 		
+		tryTraditionalInstanceOf();
+		
+		tryInstanceOfWithPatternMatching();
+		
+		tryInstanceOfWithPatternMatchingInline();
+		
 		tryPatternMatchingOnInstanceOf();
 		
+	}
+	
+	private static void tryTraditionalInstanceOf() {
+		
+		Object obj = "foo";
+		
+		if (obj instanceof String) {
+		    String str = (String) obj;
+		    System.out.println(str.toUpperCase()); // Use str
+		} else {
+			System.out.println("Unknown Type");
+		}
+
+	}
+	
+	private static void tryInstanceOfWithPatternMatching() {
+		
+		Object obj = "foo";
+		
+		if (obj instanceof String str) {
+			//str = "bar"; // Compile Error : str is final
+			System.out.println(str.toUpperCase()); // Use str
+		} else {
+			System.out.println("Unknown Type");
+		}
+
+	}
+	
+	// str is final
+	private static void tryInstanceOfWithPatternMatchingInline() {
+		
+		Object obj = "foo";
+		
+		if (obj instanceof String str && str.startsWith("foo")) {
+			System.out.println(str.toUpperCase()); // Use str
+		} else {
+			System.out.println("Unknown Type");
+		}
+
 	}
 	
 	private static void tryPatternMatchingOnInstanceOf() {
